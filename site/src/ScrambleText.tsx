294859fa12299e59e3f96fa39093d1b7649c3bb7
Jackson Warley <jtwarley@gmail.com>
2018-12-31 12:46:59 -0700

    started server stuff; async issues w scrambles

--- site/src/ScrambleText.tsx
+++ site/src/ScrambleText.tsx
@@ -1,12 +1,10 @@
 import * as React from "react";
 
-class ScrambleText extends React.Component<{}, {}> {
+class ScrambleText extends React.Component<{ scramble: string }, {}> {
     public render() {
         return (
-            <div className="f3 tc">
-                R Dw' B Dw2 Lw2 F2 D2 Lw' D U L2 F Uw Bw2 U' Fw2 U' Lw' B2 Lw2 U2 Lw' U' Uw2 D2 Lw'
-                L2 B Rw Dw U Uw' Lw' R L Fw' R2 L2 Dw Uw' D2 L' Bw' Rw' U2 D Bw U Bw2 B' L2 Fw2 B'
-                Lw L U' B' Bw2 Dw Bw2
+            <div id="scramble_area" className="f3 tc">
+                {this.props.scramble}
             </div>
         );
     }
--- site/src/Timer.tsx
+++ site/src/Timer.tsx
@@ -6,241 +6,308 @@ import ScoreCard from "./ScoreCard";
 import StatsCard from "./StatsCard";
 
 type TimerPhase =
-  | { name: "waiting" }
-  | { name: "inspecting" }
-  | { name: "green" }
-  | { name: "red"; timeTurnedRed: number }
-  | { name: "running" }
-  | { name: "stopped" };
+    | { name: "waiting" }
+    | { name: "inspecting" }
+    | { name: "green" }
+    | { name: "red"; timeTurnedRed: number }
+    | { name: "running" }
+    | { name: "stopped" };
 
 interface Model {
-  startTime: number;
-  elapsed: number;
-  phase: TimerPhase;
-  penalty?: Penalty;
-  bucket: Time[];
+    startTime: number;
+    elapsed: number;
+    phase: TimerPhase;
+    penalty?: Penalty;
+    bucket: Time[];
+    scramble: string;
+    next_scramble: string;
+}
+
+declare var puzzles: any;
+// declare var loadScript: any;
+declare global {
+    interface Window {
+        puzzles: any;
+    }
+}
+
+window.puzzles = window.puzzles || {};
+
+function getNewScramble(): Promise<string> {
+    console.log("called getnewscramble");
+    let promise: Promise<string> = new Promise(function(resolve, reject) {
+        const scram: string = window.puzzles["333"].generateScramble();
+        resolve(scram);
+    });
+    return promise;
+}
+
+async function loadScript(src: string): Promise<HTMLScriptElement> {
+    return new Promise((resolve, reject) => {
+        var tag = document.createElement("script");
+        tag.async = false;
+        tag.src = src;
+        var script = document.getElementsByTagName("head")[0].appendChild(tag);
+        resolve(script);
+    });
 }
 
 class Timer extends React.Component<{}, Model> {
-  private intervalID: number;
+    private intervalID: number;
 
-  constructor(props: {}) {
-    super(props);
-    this.state = {
-      startTime: 0,
-      elapsed: 0,
-      phase: { name: "waiting" },
-      penalty: undefined,
-      bucket: [],
-    };
-
-    this.handleKeyDown = this.handleKeyDown.bind(this);
-    this.handleKeyUp = this.handleKeyUp.bind(this);
-
-    this.intervalID = 0;
-  }
-
-  public componentDidMount() {
-    this.intervalID = window.setInterval(() => this.tick(), 1);
-
-    document.addEventListener("keydown", this.handleKeyDown);
-    document.addEventListener("keyup", this.handleKeyUp);
-  }
-
-  public componentWillUnmount() {
-    window.clearInterval(this.intervalID);
-
-    document.removeEventListener("keydown", this.handleKeyDown);
-    document.removeEventListener("keyup", this.handleKeyUp);
-  }
-
-  private handleKeyDown(event: any) {
-    this.setState((state, props) => {
-      let nextState: Model;
-      switch (state.phase.name) {
-        case "inspecting":
-          // only spacebar should ready the timer
-          nextState =
-            event.code === "Space"
-              ? {
-                  ...state,
-                  phase: { name: "red", timeTurnedRed: Date.now() },
-                }
-              : {
-                  ...state,
-                };
-          break;
-        case "running":
-          let timeToSave = { ms: state.elapsed, pen: state.penalty };
-
-          nextState = {
-            ...state,
-            phase: { name: "stopped" },
-            bucket: state.bucket.length >= 5 ? [timeToSave] : state.bucket.concat([timeToSave]),
-            // change this 5 later to the size of an avg for the event
-          };
-          break;
-        default:
-          nextState = {
-            ...state,
-          };
-          break;
-      }
-
-      return nextState;
-    });
-  }
-
-  private handleKeyUp(event: any) {
-    this.setState((state, props) => {
-      let nextState: Model;
-      switch (state.phase.name) {
-        case "waiting":
-          // only spacebar should start inspection
-          nextState =
-            event.code === "Space"
-              ? {
-                  ...state,
-                  startTime: Date.now(),
-                  phase: { name: "inspecting" },
-                }
-              : {
-                  ...state,
-                };
-          break;
-        case "red":
-          // only lifting spacebar should return to inspection
-          nextState =
-            event.code === "Space"
-              ? {
-                  ...state,
-                  phase: { name: "inspecting" },
-                }
-              : {
-                  ...state,
-                };
-          break;
-        case "green":
-          // only spacebar should start the timer
-          nextState =
-            event.code === "Space"
-              ? {
-                  ...state,
-                  startTime: Date.now(),
-                  elapsed: 0,
-                  phase: { name: "running" },
-                }
-              : {
-                  ...state,
-                };
-          break;
-        case "stopped":
-          nextState = {
-            ...state,
+    constructor(props: {}) {
+        super(props);
+        this.state = {
+            startTime: 0,
+            elapsed: 0,
             phase: { name: "waiting" },
-          };
-          break;
-        default:
-          nextState = {
-            ...state,
-          };
-          break;
-      }
+            penalty: undefined,
+            bucket: [],
+            scramble: "string from constructor",
+            next_scramble: "",
+        };
 
-      return nextState;
-    });
-  }
+        this.handleKeyDown = this.handleKeyDown.bind(this);
+        this.handleKeyUp = this.handleKeyUp.bind(this);
 
-  private tick() {
-    this.setState((state, props) => {
-      let nextState: Model;
-
-      switch (state.phase.name) {
-        case "red":
-          if (Util.timeSince(state.phase.timeTurnedRed) >= 550) {
-            nextState = {
-              ...state,
-              elapsed: Util.timeSince(state.startTime),
-              phase: { name: "green" },
-              penalty: Util.inspPenalty(state.elapsed),
-            };
-          } else {
-            nextState = {
-              ...state,
-              elapsed: Util.timeSince(state.startTime),
-              penalty: Util.inspPenalty(state.elapsed),
-            };
-          }
-          break;
-        case "inspecting":
-          nextState = {
-            ...state,
-            elapsed: Util.timeSince(state.startTime),
-            penalty: Util.inspPenalty(state.elapsed),
-          };
-          break;
-        case "green":
-          nextState = {
-            ...state,
-            elapsed: Util.timeSince(state.startTime),
-            penalty: Util.inspPenalty(state.elapsed),
-          };
-          break;
-        case "running":
-          nextState = {
-            ...state,
-            elapsed: Util.timeSince(state.startTime),
-          };
-          break;
-        default:
-          nextState = state;
-          break;
-      }
-      return nextState;
-    });
-  }
-
-  public render() {
-    // Set the color of the timer
-    let colorClass = "black";
-    if (this.state.phase.name === "red") {
-      colorClass = "red";
-    } else if (this.state.phase.name === "green") {
-      colorClass = "green";
+        this.intervalID = 0;
     }
 
-    // Pretty-print the timer readout
-    let timeString: string;
-    if (
-      this.state.phase.name === "inspecting" ||
-      this.state.phase.name === "red" ||
-      this.state.phase.name === "green"
-    ) {
-      timeString = Util.toInspString(this.state.elapsed);
-    } else {
-      timeString = Util.timeToString(this.state.elapsed);
+    public async componentDidMount() {
+        const scr = await loadScript("tnoodle.js");
+        function puzzlesLoaded(puzzles: any) {
+            console.log("puzzlesloaded called");
+            window.puzzles = puzzles;
+        }
+
+        this.setState({ scramble: "string from compenentdidmount" });
+        const scram = await getNewScramble();
+        this.setState({ scramble: scram });
+
+        this.intervalID = window.setInterval(() => this.tick(), 1);
+
+        document.addEventListener("keydown", this.handleKeyDown);
+        document.addEventListener("keyup", this.handleKeyUp);
+
+        // const promise = await getNewScramble();
+        // const scram = await promise;
+        this.setState((state, props) => {
+            return {
+                ...state,
+                scramble: scram,
+            };
+        });
     }
 
-    return (
-      <div className="flex items-start justify-between">
-        <div className="flex flex-column vh-100 justify-between w-25 outline">
-          <StatsCard />
-          <div className="outline tc">History</div>
-        </div>
+    public componentWillUnmount() {
+        window.clearInterval(this.intervalID);
 
-        <div className="flex flex-column justify-between vh-100 outline">
-          <ScrambleText />
-          <div className={"tc f1 code outline " + colorClass}>{timeString}</div>
-          <ScoreCard times={this.state.bucket} />
-        </div>
+        document.removeEventListener("keydown", this.handleKeyDown);
+        document.removeEventListener("keyup", this.handleKeyUp);
+    }
 
-        <div className="flex flex-column vh-100 justify-between w-25 outline">
-          <div className="outline tc"> Scramble image </div>
-          <div className="outline tc"> Settings </div>
-        </div>
-      </div>
-    );
-  }
+    // private writeNewScramble() {
+    //     console.log("U CALRED?");
+    //     // var scram = puzzles["333"].generateScramble();
+    //     var scramble_area: any = document.getElementById("scramble_area");
+    //     scramble_area.innerHTML = this.state.next_scramble;
+    //     // scramble_area.appendChild(document.createTextNode(scram));
+    // }
+
+    private handleKeyDown(event: any) {
+        this.setState((state, props) => {
+            let nextState: Model;
+            switch (state.phase.name) {
+                case "inspecting":
+                    // only spacebar should ready the timer
+                    nextState =
+                        event.code === "Space"
+                            ? {
+                                  ...state,
+                                  phase: { name: "red", timeTurnedRed: Date.now() },
+                              }
+                            : {
+                                  ...state,
+                              };
+                    break;
+                case "running":
+                    let timeToSave = { ms: state.elapsed, pen: state.penalty };
+
+                    nextState = {
+                        ...state,
+                        phase: { name: "stopped" },
+                        bucket:
+                            state.bucket.length >= 5
+                                ? [timeToSave]
+                                : state.bucket.concat([timeToSave]),
+                        // change this 5 later to the size of an avg for the event
+                        scramble: state.next_scramble,
+                    };
+                    break;
+                default:
+                    nextState = {
+                        ...state,
+                    };
+                    break;
+            }
+
+            return nextState;
+        });
+    }
+
+    private handleKeyUp(event: any) {
+        this.setState((state, props) => {
+            let nextState: Model;
+            switch (state.phase.name) {
+                case "waiting":
+                    // only spacebar should start inspection
+                    nextState =
+                        event.code === "Space"
+                            ? {
+                                  ...state,
+                                  startTime: Date.now(),
+                                  phase: { name: "inspecting" },
+                              }
+                            : {
+                                  ...state,
+                              };
+                    break;
+                case "red":
+                    // only lifting spacebar should return to inspection
+                    nextState =
+                        event.code === "Space"
+                            ? {
+                                  ...state,
+                                  phase: { name: "inspecting" },
+                              }
+                            : {
+                                  ...state,
+                              };
+                    break;
+                case "green":
+                    // start the solve phase
+                    nextState =
+                        event.code === "Space" // only spacebar should start the timer
+                            ? {
+                                  ...state,
+                                  startTime: Date.now(),
+                                  elapsed: 0,
+                                  phase: { name: "running" },
+                              }
+                            : {
+                                  ...state,
+                              };
+                    // cache the next scramble while the timer is running
+                    // let promise = getNewScramble();
+                    nextState.next_scramble = "HUKKKKAKAKAKAKAKA";
+                    break;
+                case "stopped":
+                    nextState = {
+                        ...state,
+                        phase: { name: "waiting" },
+                    };
+                    break;
+                default:
+                    nextState = {
+                        ...state,
+                    };
+                    break;
+            }
+
+            return nextState;
+        });
+    }
+
+    private tick() {
+        this.setState((state, props) => {
+            let nextState: Model;
+
+            switch (state.phase.name) {
+                case "red":
+                    if (Util.timeSince(state.phase.timeTurnedRed) >= 550) {
+                        nextState = {
+                            ...state,
+                            elapsed: Util.timeSince(state.startTime),
+                            phase: { name: "green" },
+                            penalty: Util.inspPenalty(state.elapsed),
+                        };
+                    } else {
+                        nextState = {
+                            ...state,
+                            elapsed: Util.timeSince(state.startTime),
+                            penalty: Util.inspPenalty(state.elapsed),
+                        };
+                    }
+                    break;
+                case "inspecting":
+                    nextState = {
+                        ...state,
+                        elapsed: Util.timeSince(state.startTime),
+                        penalty: Util.inspPenalty(state.elapsed),
+                    };
+                    break;
+                case "green":
+                    nextState = {
+                        ...state,
+                        elapsed: Util.timeSince(state.startTime),
+                        penalty: Util.inspPenalty(state.elapsed),
+                    };
+                    break;
+                case "running":
+                    nextState = {
+                        ...state,
+                        elapsed: Util.timeSince(state.startTime),
+                    };
+                    break;
+                default:
+                    nextState = state;
+                    break;
+            }
+            return nextState;
+        });
+    }
+
+    public render() {
+        // Set the color of the timer
+        let colorClass = "black";
+        if (this.state.phase.name === "red") {
+            colorClass = "red";
+        } else if (this.state.phase.name === "green") {
+            colorClass = "green";
+        }
+
+        // Pretty-print the timer readout
+        let timeString: string;
+        if (
+            this.state.phase.name === "inspecting" ||
+            this.state.phase.name === "red" ||
+            this.state.phase.name === "green"
+        ) {
+            timeString = Util.toInspString(this.state.elapsed);
+        } else {
+            timeString = Util.timeToString(this.state.elapsed);
+        }
+
+        return (
+            <div className="flex items-start justify-between">
+                <div className="flex flex-column vh-100 justify-between w-25 outline">
+                    <StatsCard />
+                    <div className="outline tc">History</div>
+                </div>
+
+                <div className="flex flex-column justify-between vh-100 outline">
+                    <ScrambleText scramble={this.state.scramble} />
+                    <div className={"tc f1 code outline " + colorClass}>{timeString}</div>
+                    <ScoreCard times={this.state.bucket} />
+                </div>
+
+                <div className="flex flex-column vh-100 justify-between w-25 outline">
+                    <div className="outline tc"> Scramble image </div>
+                    <div className="outline tc"> Settings </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Timer;
